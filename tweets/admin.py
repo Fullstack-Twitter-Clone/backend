7b2180dffbbd07e568f1d288d1a2930c28a158d9
James-Tepper <jamesatepper@gmail.com>
2023-02-21 09:53:33 -0500

    first commit

---
+++ tweets/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Tweet # ! TODO
+
+admin.site.register(Tweet)
---
+++ users/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def home(request):
+    return render('You are at the Users index page')
